ad874648d9dc750b74bb5c00c18fe0181673a0cf
BeaverDuck <beaverduck99@gmail.com>
2018-06-24 23:14:44 -0400

    Fixed global debugging

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
-    client.channels.get(457201559772594211).send
+    client.channels.get('457201559772594211').send('Test')
     res.render('fileReq')
 })
 
