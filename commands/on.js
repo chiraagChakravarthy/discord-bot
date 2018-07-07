ef0bbeaf5f3570c9088de6ea4b04767fa9d43eb7
BeaverDuck <beaverduck99@gmail.com>
2018-07-06 20:51:19 -0400

    Rerouted token to private config file

--- commands/on.js
+++ commands/on.js
@@ -1,6 +1,6 @@
-const fs = require('fs'),
-    config = JSON.parse(fs.readFileSync(__dirname + '/../config.json')),
-    prefix = config.prefix
+
+var prefix = process.env.PREFIX
+
 module.exports = function(client, command, fun){
     client.on('message', message =>{
         var split = message.content.split(' ')
